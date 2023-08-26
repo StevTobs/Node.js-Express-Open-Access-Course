7c22f81513475d96474385bdb68b259a4a1d0a0d
Akanit Kwangkaew <akanitk84@gmail.com>
2023-08-26 11:08:20 +0700

    initial

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const debug = require('debug')('app');
 // for run (Windows):set DEBUG=* & node app.js
  
 const morgan = require('morgan');
+const path = require('path');
+
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) => {
     res.send('Hello World!');
