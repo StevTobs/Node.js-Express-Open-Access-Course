12fe0d53377fa1672b64ab33fd0009462024bf05
Akanit Kwangkaew <akanitk84@gmail.com>
2024-05-02 23:51:31 +0700

    add

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+
 const chalk = require('chalk');
 // npm install chalk@4.0.0
 
@@ -10,19 +12,25 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'))
+
 app.use(express.static(path.join(__dirname,"/public/")))
 
-app.get("/", (req,res) => {
-    res.send('Hello World!');
+// app.get("/", (req,res) => {
+//     res.send('The engine is powered by Node.js.\nCreated by Akanit Kwangkaew');
+// })
+app.get('/', (req, res) => {
+    // Including newline characters in the response
+    const responseText = 'Hello, this is powered by Node.js.<br>Created by Akanit.';
+    res.send(responseText);
+  });
+  
+  
+app.listen(PORT, ()=>{
+    debug("Listening on PORT : " + chalk.green(PORT));
 })
 
-app.listen(port, ()=>{
-    // console.log("Listening on port : " + chalk.green(port));
-    debug("Listening on port : " + chalk.green(port));
-})
-
+// To run app : node app.js   
---
+++ note.md
@@ -0,0 +1,21 @@
+# Node.js Cheat Sheet
+
+This cheat sheet provides a quick reference for common Node.js commands, operations, and package management.
+
+## Installation
+
+### Install Node.js
+
+Download and install Node.js from the official website: [Node.js](https://nodejs.org/)
+
+## Getting Started
+
+### Create a new Node.js project
+
+```bash
+mkdir my_project
+cd my_project
+npm init -y
+
+
+> code
